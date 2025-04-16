68ca9f6361197af7b56cc8575979b1c6e03e44fb
David Valentine <david.valentine@gmail.com>
2025-04-15 22:19:44 -0700

    Update ISO8601Date, SchemaOrgBase with pydantic.v1 add jinja2

--- pydantic_schemaorg/ISO8601/ISO8601Date.py
+++ pydantic_schemaorg/ISO8601/ISO8601Date.py
@@ -2,7 +2,7 @@ import re
 from typing import no_type_check, Optional, Dict, cast, Any, Pattern, TYPE_CHECKING, Generator, AnyStr, Union
 
 from pydantic import BaseConfig
-from pydantic.fields import ModelField
+from pydantic.v1.fields import ModelField
 from pydantic.utils import update_not_none
 from pydantic.validators import str_validator, constr_length_validator
 
